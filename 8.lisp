294a5b2fcb65cae6f15f64d0f3e88507aaa723d5
cympfh <cympfh@gmail.com>
2014-10-18 18:21:22 +0900

    episode 8 not finished

---
+++ 8.lisp
@@ -0,0 +1,163 @@
+(load "graph-util")
+
+(defparameter *congestion-city-nodes* nil)
+(defparameter *congestion-city-edges* nil)
+(defparameter *visited-nodes* nil)
+(defparameter *node-num* 30)
+(defparameter *edge-num* 45)
+(defparameter *worm-num* 3)
+(defparameter *cop-odds* 15)
+(defparameter *player-pos* 1)
+
+(defun random-node ()
+  (1+ (random *node-num*)))
+
+(defun edge-pair (a b)
+  (unless (equal a b)
+    (list (cons a b)
+          (cons b a))))
+
+(defun make-edge-list ()
+  (apply #'append
+         (loop repeat *edge-num*
+               collect (edge-pair (random-node)
+                                  (random-node)))))
+
+;; find not connected nodes (islands, 孤島)
+
+(defun direct-edges (node edge-list)
+  (remove-if-not (lambda (edge) (equal (car edge) node))
+                 edge-list))
+
+(defun get-connected (node edge-list)
+  (let ((visited nil))
+    (labels
+      ((traverse (node)
+         (unless (member node visited)
+           (push node visited)
+           (mapc (lambda (edge) (traverse (cdr edge)))
+                 (direct-edges node edge-list)))))
+      (traverse node))
+    visited))
+
+(defun find-islands (nodes edge-list)
+  (let ((islands nil))
+    (labels
+      ((find-island (nodes)
+        (let* ((connected (get-connected (car nodes)
+                                         edge-list))
+               (uconnected (set-difference nodes
+                                           connected)))
+          (push connected islands)
+          (when uconnected
+            (find-island uconnected)))))
+      (find-island nodes))
+    islands))
+
+(defun connect-with-bridges (islands)
+  (when (cdr islands)
+    (append (edge-pair (caar islands) (caadr islands))
+            (connect-with-bridges (cdr islands)))))
+
+(defun connect-all-islands (nodes edge-list)
+  (append (connect-with-bridges (find-islands nodes edge-list)) edge-list))
+
+;; conjestion city
+
+(defun make-city-edges ()
+  (let* ((nodes (loop for i from 1 to *node-num*
+                      collect i))
+         (edge-list (connect-all-islands nodes
+                                         (make-edge-list)))
+         (cops (remove-if-not
+                 (lambda (x) (zerop (random *cop-odds*)))
+                 edge-list)))
+    (add-cops (edges-to-alist edge-list) cops)))
+
+;; ((1 . 2) (1 . 3) (3 . 2)) -> ((1 (2 3)) (3 (2)))
+(defun edges-to-alist (edge-list)
+  (let ((nodes (remove-duplicates
+                 (mapcar #'car edge-list))))
+    (mapcar
+      (lambda (node1)
+        (cons node1
+              (mapcar (lambda (edge)
+                        (list (cdr edge)))
+                      (remove-duplicates
+                        (direct-edges node1 edge-list)
+                        :test #'equal))))
+        nodes)))
+
+(defun add-cops (edge-alist edges-with-cops)
+  ; (print 'add-cops)
+  ; (print edge-alist)
+  ; (print edges-with-cops)
+  (mapcar (lambda (x)
+            (let ((node1 (car x))
+                  (node1-edges (cdr x)))
+              (cons node1
+                    (mapcar (lambda (edge)
+                              (let ((node2 (car edge)))
+                                (if (intersection (edge-pair node1 node2)
+                                                  edges-with-cops
+                                                  :test #'equal)
+                                  (list node2 'cops)
+                                  edge)))
+                            node1-edges))))
+          edge-alist))
+
+(defun neighbors (node edge-alist)
+  (mapcar #'car (cdr (assoc node edge-alist))))
+
+(defun within-one (a b edge-alist)
+  (member b (neighbors a edge-alist)))
+
+(defun within-two (a b edge-alist)
+  (or (within-one a b edge-alist)
+      (some (lambda (x)
+              (within-one x b edge-alist))
+            (neighbors a edge-alist))))
+
+(defun make-city-nodes (edge-alist)
+  (let ((wumpus (random-node))
+        (glow-worms (loop for i below *worm-num*
+                          collect (random-node))))
+    (loop for n from 1 to *node-num*
+          collect
+          (append (list n)
+                  (cond ((equal n wumpus) '(wumps))
+                        ((within-two n wumpus edge-alist)
+                         '(blood)))
+                  (cond ((member n glow-worms)
+                         '(glow-worms))
+                        ((some (lambda (worm) (within-one n worm edge-alist))
+                               glow-worms)
+                         '(light)))
+                  (when (some #'cdr (cdr (assoc n edge-alist)))
+                    '(sirens))))))
+
+;; 8.4
+
+(defun new-game ()
+  (setf *congestion-city-edges* (make-city-edges))
+  (setf *congestion-city-nodes*
+        (make-city-nodes *congestion-city-edges*))
+  (print *congestion-city-edges*)
+  (print 'mark)
+  (setf *player-pos* (find-empty-node))
+  (print 'mark)
+  (setf *visited-nodes* (list *player-pos*))
+  (print 'mark)
+  (draw-city))
+
+(defun find-empty-node ()
+  (let ((x (random-node)))
+    (if (cdr (assoc x *congestion-city-nodes*))
+      (find-empty-node)
+      x)))
+
+(defun draw-city ()
+  (ugraph->png "city" *congestion-city-nodes*
+               *congestion-city-edges*))
+
+;; akita
